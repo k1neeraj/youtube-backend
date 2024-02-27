0c404102f9d0b3c5a17e39004bfe4b30d6a5d3fc
Neeraj Kumar <k1_neeraj@yahoo.com>
2024-02-27 12:03:49 +0530

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -48,7 +48,7 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
@@ -65,18 +65,17 @@ userSchema.methods.generateAccessToken = function () {
       fullName: this.fullName,
     },
     process.env.ACCESS_TOKEN_SECRET,
-    {expiresIn: process.env.ACCESS_TOKEN_EXPIRY},
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY },
   );
 };
 userSchema.methods.generateRefreshToken = function () {
-    return jwt.sign(
-        {
-          _id: this._id,
-          
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {expiresIn: process.env.REFRESH_TOKEN_EXPIRY},
-      );
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY },
+  );
 };
 
 export const User = mongoose.model("User", userSchema);
